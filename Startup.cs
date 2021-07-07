e6fe5a303578c9a61b077974b223cb4028214593
LouisKamp <louiskampeskildsen99@gmail.com>
2021-07-07 14:31:39 +0200

    cleanup

--- Startup.cs
+++ Startup.cs
@@ -31,8 +31,6 @@ namespace dotnetgraphql
                             .UseLazyLoadingProxies()
             );
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
